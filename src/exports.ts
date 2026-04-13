d990cc6870fabbe24b5d966acc518577f9b124a2
kushalshit27 <43465488+kushalshit27@users.noreply.github.com>
2026-04-13 11:16:09 +0530

    fix(exports.ts): remove trailing comma in export statement

--- src/exports.ts
+++ src/exports.ts
@@ -1,5 +1,5 @@
 export { TOOLS, HANDLERS } from './tools/index.js';
-export { packageName, packageVersion, packageInfo,  } from './utils/package.js';
+export { packageName, packageVersion, packageInfo } from './utils/package.js';
 export { getToolsForScopes } from './utils/scopes.js';
 export { getTools, getHandlers } from './utils/tools.js';
 export { ServerMode } from './utils/types.js';
