0450e0c86694e92f1c1527f535d4b65193169e23
GilbertLS <gilbert_ls@hotmail.com>
2026-06-04 14:02:04 -0400

    chore: fix formatting in onboarding tool and exports test

--- test/exports.test.ts
+++ test/exports.test.ts
@@ -9,7 +9,11 @@ describe('exports', () => {
   });
 
   it('all other tools are not marked localOnly', () => {
-    const localOnlyToolNames = ['auth0_save_credentials_to_file', 'auth0_get_quickstart_guide', 'auth0_onboarding'];
+    const localOnlyToolNames = [
+      'auth0_save_credentials_to_file',
+      'auth0_get_quickstart_guide',
+      'auth0_onboarding',
+    ];
     const localOnlyTools = TOOLS.filter(
       (t) => !localOnlyToolNames.includes(t.name) && t._meta?.localOnly
     );
